1ec47e8c9b6db49f64cdc4518e4ed318b3d627eb
PatrykKlimas <patryk.klimas@onet.pl>
2021-02-10 23:08:27 +0100

    Init Commit

---
+++ Web Task Manager/Models/TaskModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Web_Task_Manager.Models
+{
+    [Table("Tasks")]
+    public class TaskModel
+    {
+        [Key]
+        public int TaskID { get; set; }
+        [DisplayName("Task Name")]
+        [Required(ErrorMessage = "Field name is requied.")]
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [DisplayName("Task Description")]
+        [MaxLength(2000)]
+        public string Description { get; set; }
+        public bool Done { get; set; }
+    }
+}
---
+++ Web Task Manager/Repositories/ITaskRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Web_Task_Manager.Models;
+
+namespace Web_Task_Manager.Repositories
+{
+    public interface ITaskRepository
+    {
+        TaskModel Get(int taskID);
+        IQueryable<TaskModel> GetAllActive();
+        void Add(TaskModel task);
+        void Update(int taskID, TaskModel task);
+        void Delete(int taskID);
+    }
+}
